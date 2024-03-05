c23bbc8dfc3e8d5af9c72c703b509752af5b49e4
Adrian Espinoza <adrian_espinn1324@hotmail.com>
2024-03-04 21:10:44 -0600

    correccion en bd

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 export const connectDB = async () =>{
     try{
-        await mongoose.connect("mongodb://localhost/eolicPoweruuuuDb");
+        await mongoose.connect("mongodb://localhost/eolicPowerDb");
         console.log("db is connected")
     }catch(error){
         console.log(error);
