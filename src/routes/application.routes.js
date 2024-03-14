8944afc58c7f86c251151142884228729f7ca683
Adrian Espinoza <adrian_espinn1324@hotmail.com>
2024-03-14 09:42:35 -0600

    Limitar las peticiones de acceso

--- src/routes/application.routes.js
+++ src/routes/application.routes.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import {authRequired} from '../middlewares/validateToken.js'
 import { getApplication,getApplications,createApplications,updateApplications,deleteApplications } from "../controllers/application.controller.js";
+import { rateLimit } from "express-rate-limit";
+
+
+const createApplicationMessage = "Usted ya ha sobrepasado el limite de solicitudes.";
+
+
+
+const createApplicationLimiter = rateLimit({
+    windowMs: 24 * 24 * 60 * 60 * 1000, //1 día
+    max: 2, //2 peticiones por mes, se usa 24 para no sobrepasar el valor maximo d 32 bits
+    message: createApplicationMessage,
+});
+
 
 const router = Router();
 
 router.get('/applications', authRequired,getApplications);
 router.get('/applications/:id', authRequired,getApplication);
-router.post('/applications', authRequired,createApplications);
+router.post('/applications',createApplicationLimiter, authRequired,createApplications);
 router.delete('/applications/:id', authRequired,deleteApplications);
 router.put('/applications/:id', authRequired,updateApplications);
 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,36 @@
 import { Router } from "express";
-import { login, register,logout,userProfile} from '../controllers/auth.controller.js';
-import {authRequired} from '../middlewares/validateToken.js'
+import { login, register, logout, userProfile } from "../controllers/auth.controller.js";
+import { authRequired } from "../middlewares/validateToken.js";
+import { rateLimit } from "express-rate-limit";
+
+const loginMessage = "Demasiados intentos de inicio de sesión. Intenta de nuevo más tarde.";
+const userMessage = "Demasiadas peticiones. Intenta de nuevo más tarde.";
+const createUserMessage = "Solo puedes crear 2 cuentas por día.";
+
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000, //1 minuto
+  max: 5, //5 peticiones por minuto
+  message: loginMessage,
+});
+
+const userLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, //1 hora
+  max: 10, //10 peticiones por hora
+  message: userMessage,
+});
+
+const createUserLimiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, //1 día
+  max: 2, //2 peticiones por día
+  message: createUserMessage,
+});
+
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', logout);
+router.post("/register", createUserLimiter, register);
+router.post("/login", loginLimiter, login);
+router.post("/logout", logout);
 
-router.get("/userProfile",authRequired,userProfile);
+router.get("/userProfile", authRequired, userProfile);
+
 export default router;
