ca53ca2d5897d3829448e1636b3e6ce48c680edb
Adrian Espinoza <adrian_espinn1324@hotmail.com>
2024-04-17 22:41:22 -0600

    xxxx

--- src/db.js
+++ src/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () =>{
     try{
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect("mongodb://54.227.142.31:27017/eolicPowerDb");
         console.log("db is connected")
     }catch(error){
         console.log(error);
